f1bd2b14bf0ef16e438f347c164615429319cda9
Denisa Danci <denisadanci00@gmail.com>
2021-11-28 23:55:21 +0200

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Danci_Denisa_Lab8.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
